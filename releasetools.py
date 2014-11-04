e554e0c892c69e6ab2a06820a896edb446826e48
jchjian <671346@qq.com>
2014-11-04 22:05:58 +0800

    update selfinfo for updater-script

--- releasetools.py
+++ releasetools.py
@@ -1,12 +1,19 @@
 import common
 import edify_generator
-
+import time
 
 def MyselfInfo(info):
   edify = info.script
   for i in xrange(len(edify.script)):
     if "show_progress(" in edify.script[i] and "0.500000" in edify.script[i]:
-      edify.script[i] = 'ui_print("========================");\nui_print("    weibo.com/7170884   ");\nui_print("        by yegui        ");\nui_print("========================");\nshow_progress(0.500000, 0);'
+      edify.script[i] = '''ui_print("========================");
+ui_print("          LA-Q1         ");
+ui_print("         Baidu OS       ");
+ui_print("        ''' + time.strftime('%Y-%m-%d',time.localtime(time.time())) + '''      ");
+ui_print("    weibo.com/7170884   ");
+ui_print("        by yegui        ");
+ui_print("========================");
+show_progress(0.500000, 0);'''
       return
 
 
